dbac94e52eedc7bfc4cf1de03c46fd1792b31dc5
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-11-23 10:36:33 -0300

    add: scanned vehicles counter

--- script.js
+++ script.js
@@ -7,31 +7,36 @@ document.getElementById('Check missing images').addEventListener('click', async 
 });
 
 function callFindUrlsAndModels() {
-	const instances = findUrlsAndModels();
+	const { instances, message } = findUrlsAndModels();
+	console.log(message);
 	console.log(instances);
 
 	function findUrlsAndModels() {
 		const elements = document.querySelectorAll('.vehicle-car__section');
 		const result = [];
-
+	
+		let scannedVehicles = 0;
+	
 		elements.forEach(element => {
+			scannedVehicles++;
+	
 			const modelElement = element.querySelector('.value__model');
 			const trimElement = element.querySelector('.value__trim');
 			const stockNumberElement = element.querySelector('.stock_number');
 			const imageUrlElement = element.querySelector('.main-img');
-
+	
 			if (modelElement && trimElement && stockNumberElement && imageUrlElement) {
 				const model = modelElement.textContent.trim();
 				const trim = trimElement.textContent.trim();
 				const stockNumber = stockNumberElement.textContent.trim();
 				const imageUrl = imageUrlElement.dataset.src;
-
+	
 				if (imageUrl && (imageUrl.includes('better-photo.jpg') || imageUrl.includes('spinner.gif'))) {
 					result.push({ model, trim, stockNumber, imageUrl });
 				}
 			}
-			window.scrollTo(0, document.body.scrollHeight);
-		});
-		return result;
+		});	
+		const message = `Scanned ${scannedVehicles} vehicle${scannedVehicles !== 1 ? 's' : ''}.`;	
+		return { instances: result, message };
 	}
 }
