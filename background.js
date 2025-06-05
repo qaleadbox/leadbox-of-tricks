603f5a8edb81444ee0891c711b8ff546ac4f2fa3
robinsonmourao <bob.info.guaratiba@gmail.com>
2025-06-05 15:00:48 -0300

    logo changed

--- background.js
+++ background.js
@@ -4,9 +4,7 @@ const icons = [
   "./icons/16x16/icon1.png",
   "./icons/16x16/icon2.png",
   "./icons/16x16/icon3.png",
-  "./icons/16x16/icon4.png",
-  "./icons/16x16/icon5.png",
-  "./icons/16x16/icon6.png"
+  "./icons/16x16/icon4.png"
 ];
 let currentIndex = 0;
 
